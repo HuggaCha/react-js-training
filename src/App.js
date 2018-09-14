a8e9dc0970d87ed71ba8f7073c5851a866ae86fe
HuggaCha <eminov_murad@mail.ru>
2018-09-14 20:15:08 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -50,16 +50,35 @@ togglePersonsHandler = () => {
   });
 }
 
-render() {
+
+
+render () {
 
  const style = {
-  backgroundColor: 'white', 
+  backgroundColor: 'white',
   font: 'inherit',
-  border: '1px solid blue', 
+  border: '1px solid blue',
+  borderRadius: '5px', 
   padding: '8px', 
   cursor: 'pointer',
   color: 'red'
  };
+
+ let persons = null; 
+
+ if (this.state.showPersons){
+
+  persons = (
+     <div>
+          {this.state.persons.map(person => {
+            return <Person name={person.name}
+            age={person.age} />
+          })}
+           
+    </div> 
+    );
+
+ }
 
 return (
       <div className="App">
@@ -68,25 +87,9 @@ return (
       <button 
       style = {style}
       onClick = {this.togglePersonsHandler}> Switch Name </button> 
-      
-      {this.state.showPersons === true ?
-          <div>
-           <Person 
-           name = {this.state.persons[0].name} 
-           age = {this.state.persons[0].age}
-           click = {this.switchNameHandler.bind(this,"Echo 1")} />
-          <Person 
-           name = {this.state.persons[1].name} 
-           age = {this.state.persons[1].age}
-           click = {this.switchNameHandler.bind(this, 'Murka')}
-           changed = {this.nameChangedHandler}> 
-           My Hobbies: Racing 
-           </Person> 
-          <Person 
-           name = "Stephanie" 
-           age = "26" />
-          </div> : null
-      }
+      {persons}
+     
+             
       
      </div>
    );
